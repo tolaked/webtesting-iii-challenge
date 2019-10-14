b9bda76d39c0783b7d12858e661ceb309bb16c85
Akere Adetola <adetolaakere1@gmail.com>
2019-10-14 15:03:10 +0100

    dashboard test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,27 @@
-// Test away
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Dashboard />);
+});
+describe("Dashboard component", () => {
+  test("matches the snapshot!", () => {
+    expect(wrapper.container).toMatchSnapshot();
+  });
+
+  it("displays the lock display", () => {
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+  });
+
+  it("displays the controls", () => {
+    expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
+  });
+});
